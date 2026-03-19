6d5852ee2a0571db6cfcd11fca382e33611a2150
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2026-03-19 17:11:11 -0300

    feat: little changes

--- backend/src/main/java/com/example/Tarefator/TarefatorApplication.java
+++ backend/src/main/java/com/example/Tarefator/TarefatorApplication.java
@@ -1,6 +1,5 @@
 package com.example.Tarefator;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
